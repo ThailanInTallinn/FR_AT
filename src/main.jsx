05b1f7d0bc9aa2da5b1fb3f9215314b038142f17
Thailan Gouvea <thailangm@icloud.com>
2024-09-23 15:34:39 +0300

    Updates main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Routes from "./App.jsx";
+import { HashRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<Routes />);
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
